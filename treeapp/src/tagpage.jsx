f57b787079bb263dd9e15d688816b4ec6445515f
mzmero <m.zmiro@hotmail.com>
2020-04-01 14:42:23 +0300

    fix bug

--- treeapp/src/tagpage.jsx
+++ treeapp/src/tagpage.jsx
@@ -1,28 +1,27 @@
-import 'react-sortable-tree/style.css';
 import React, { Component } from 'react';
 import SortableTree from 'react-sortable-tree';
-//import data from './dataset.csv'
+import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
 
 export default class Tree extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      treeData: [
-        { title: 'Chicken', children: [{ title: 'Egg' }] },
-        { title: 'Fish', children: [{ title: 'fingerline' }, { title: 'frenzy' }] },
-        { title: 'frenzy', children: [{ title: 'finger' }] }
-      ],
-    };
-  }
+    constructor(props) {
+        super(props);
 
-  render() {
-    return (
-      <div style={{ height: 400 }} >
-        <SortableTree
-          treeData={this.state.treeData}
-          onChange={treeData => this.setState({ treeData })}
-        />
-      </div >
-    );
-  }
+        this.state = {
+            treeData: [
+                { title: 'Chicken', children: [{ title: 'Egg' }] },
+                { title: 'Fish', children: [{ title: 'fingerline' }] }
+            ],
+        };
+    }
+
+    render() {
+        return (
+            <div style={{ height: 400 }}>
+                <SortableTree
+                    treeData={this.state.treeData}
+                    onChange={treeData => this.setState({ treeData })}
+                />
+            </div>
+        );
+    }
 }
