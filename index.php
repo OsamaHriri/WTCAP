f067112f427a1e8ed3a2f0ba255300a9de5afb1d
Nareed <45772161+Nareed@users.noreply.github.com>
2020-04-01 14:03:09 +0300

    A little change

---
+++ index.php
@@ -0,0 +1,39 @@
+<html lang="ar">
+
+<head>
+    <meta charset="utf-8">
+    <title>Semantic Arabic Tagging</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script src="js/main-page-js.js"></script>
+</head>
+
+<body>
+    <div>
+        <?php include "mynav.php"; ?>
+    </div>
+    <div id=main>
+        <h2> Main page </h2>
+        <p> one long para </p>
+
+        <table id="poem">
+            <tr>
+                <td> لَوَتْ بِالسَّلامِ بَنَانًا خَضيبا </td>
+                <td>وَلَحْظًا يشوقُ الفؤادَ الطَّرُوبا</td>
+            </tr>
+            <tr>
+                <td>وَزَارَتْ على عَجَلٍ فاكْتَسى</td>
+                <td>لِزَوْرَتِها ((أَبْرَقُ الحَزْنِ)) طيبا</td>
+            </tr>
+            <tr>
+                <td>فكانَ العبيرُ بها وَاشِيًا</td>
+                <td>وَجَرْسُ الحُلِيِّ عليها رَقيبا</td>
+            </tr>
+            <tr>
+                <td>وَلَمْ أَنْسَ لَيْلَتَنَا في العِنَا</td>
+                <td>قِ لَفَّ الصَّبَا بِقَضيبٍ قَضيبا </td>
+            </tr>
+        </table>
+    </div>
+</body>
+
+</html>
