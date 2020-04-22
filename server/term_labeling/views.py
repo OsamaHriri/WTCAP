7a52e914b86b981d9b192ec63f63a57582e46255
Nareed <nareedhashem@hotmail.com>
2020-04-22 15:46:00 +0300

    added dummy database and  selection button works now

--- server/term_labeling/views.py
+++ server/term_labeling/views.py
@@ -9,9 +9,115 @@ from subprocess import run, PIPE
 
 # Create your views here.
 
+# create dummy data of line and context
+
+poem = [
+    {
+        'index': '1',
+        'content': {
+            'first': 'لَوَتْ بِالسَّلامِ بَنَانًا خَضيبا',
+            'second': 'وَلَحْظًا يشوقُ الفؤادَ الطَّرُوبا'
+        }
+    },
+    {
+        'index': '2',
+        'content': {
+            'first': 'َزَارَتْ على عَجَلٍ فاكْتَسى',
+            'second': 'لِزَوْرَتِها ((أَبْرَقُ الحَزْنِ)) طيبا'
+        }
+    },
+    {
+        'index': '3',
+        'content': {
+            'first': 'فكانَ العبيرُ بها وَاشِيًا',
+            'second': 'وَجَرْسُ الحُلِيِّ عليها رَقيبا'
+        }
+    },
+    {
+        'index': '4',
+        'content': {
+            'first': 'وَلَمْ أَنْسَ لَيْلَتَنَا في العِنَا',
+            'second': 'وقِ لَفَّ الصَّبَا بِقَضيبٍ قَضيبا '
+        }
+    }, {
+        'index': '5',
+        'content': {
+            'first': 'سُكُوتٌ يَحرُّ عليهِ الهوى',
+            'second': 'وشكوى تَهِيجُ البُكا والنَّحيبا'
+        }
+    },
+    {
+        'index': '6',
+        'content': {
+            'first': 'َمَا افْتَنَّتِ الرّيحُ فِي مَرِّها',
+            'second': 'فَطَوْرًا خُفُوتًا، وَطَوْرًا هُبُوبا'
+        }
+    },
+    {
+        'index': '7',
+        'content': {
+            'first': 'عَنَتْ كَبِدي قَسْوةٌ منكِ ما',
+            'second': 'تَزالُ تُجدِّدُ فيها نُدُوبا'
+        }
+    },
+    {
+        'index': '8',
+        'content': {
+            'first': 'وَحُمِّلْتُ عِنْدَكِ ذَنْبَ المَشي',
+            'second': 'بِ، حَتّى كَأَنّي ابْتَدَعْتُ المَشِيبا'
+        }
+    },
+    {
+        'index': '9',
+        'content': {
+            'first': ' وَمَنْ يَطَّلِعْ شَرَفَ الأَرْبَعِي ',
+            'second': 'ن يُحَيِّ مِنَ الشَّيْبِ زَوْرًا غريبا'
+        }
+    },
+    {
+        'index': '10',
+        'content': {
+            'first': ' بَلَوْنا ضَرَائبَ مَنْ قَدْ نَرى',
+            'second': 'فَما إِنْ رَأَيْنا لِ((فَتْحٍ)) ضَرِيبا'
+        }
+    },
+    {
+        'index': '11',
+        'content': {
+            'first': 'هُوَ المَرْءُ أَبْدَتْ لَهُ الحَادِثا ',
+            'second': ' تُ عَزْمًا وَشِيكًا وَرَأْيًا صَلِيبا '
+        }
+    },
+    {
+        'index': '12',
+        'content': {
+            'first': 'تَنَقَّل في خُلُقَى سُؤْدُدٍ:',
+            'second': ' سَمَاحًا مَرُجًّى، وَبَأْسًا مَهِيبا '
+        }
+    },
+]
+
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'poems': poem
+    }
+    return render(request, 'index.html', context)
+
+
+def process_lines(request):
+    selected = []
+    if request.method == 'POST':
+        chosen_lines = list(request.POST.values())
+        for value in poem.__iter__():
+            if value.__getitem__('index') in chosen_lines:
+                print('this index exists')
+                selected.append(value)
+                print(selected)
+    context = {
+        'selected': selected
+    }
+    return render(request, 'process_lines.html', context)
 
 
 def button(request):
@@ -46,5 +152,5 @@ def external2(request):
         if p is not None:
             break
     if p is not None:
-       return render(request, 'home.html', {'data1': p})
-    return render(request, 'home.html', {'data1':'not found'})
+        return render(request, 'home.html', {'data1': p})
+    return render(request, 'home.html', {'data1': 'not found'})
