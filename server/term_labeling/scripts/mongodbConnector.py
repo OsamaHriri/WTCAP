a74a04d277b3e2cf4aaf57758e15a2e1fc2c91d1
mzmero <m.zmiro@hotmail.com>
2021-02-17 17:40:09 +0200

    user history

--- server/term_labeling/scripts/mongodbConnector.py
+++ server/term_labeling/scripts/mongodbConnector.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 import os
 import pymongo
-
+import time
 """  
    connector class, pre-made to ease the access to the mongodb database, 
    inorder for this claas to work, the database must be up and running
@@ -16,9 +16,14 @@ class Connector:
         self.db = self.client["arabic_semantic_labeling"]
         self.poemsCollections = self.db["poems"]
         self.poetsCollections = self.db["poets"]
+        self.periodsCollections = self.db["period"]
+        self.userHistoryCollections = self.db["userHistory"]
 
     def get_poem(self, poemID):
         return list(self.poemsCollections.find({"id": str(poemID)}))
+
+    def get_poet_name(self, poet_id):
+        return list(self.poetsCollections.find({"id": poet_id}, {"_id": 0, 'name':1}))
 
     def get_poems_by_poet(self, poet_id):
         return list(self.poemsCollections.find({"poet_id": poet_id}, {"_id": 0, "id": 1, "name": 1}))
@@ -45,6 +50,16 @@ class Connector:
                                   'pipeline':[{"$match":{ "$expr": { "$eq": ["$poet_id", "$$id"] }}},{'$project':{'_id':0,'context':{'sadr':1,'ajuz':1}}}],
                                             'as': 'results' }}])
         return list(result)
+
+    def add_user_entry(self, user_id, poem_id ,poem_name ,poet_name):
+        _dict = {'user_id': user_id, 'poem_id': poem_id, 'time': time.time() ,'poem_title': poem_name,'poet_name':poet_name}
+        self.userHistoryCollections.insert_one(_dict)
+
+    def get_user_history(self, user_id):
+        return list(self.userHistoryCollections.find({'user_id': user_id}, {'_id': 0,'poem_id': 1, 'time': 1 , 'poem_title':1 ,'poet_name':1}))
+
+    def get_all_user_history(self):
+        return list(self.userHistoryCollections.find({},{'poem_id': 1, 'time': 1, 'poem_title': 1, 'poet_name': 1,'user_id':1}))
 
 
 
